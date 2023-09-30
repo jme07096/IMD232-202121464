fec6ee7f78541c8ffa0e23ab64d3f9cc595b01ce
jme07096 <143993256+jme07096@users.noreply.github.com.>
2023-10-01 04:36:06 +0900

    4 step1

--- src/w4/sketches/step1/script/sketch.js
+++ src/w4/sketches/step1/script/sketch.js
@@ -1,34 +1,15 @@
-let ball;
-let ball2;
-let gravity;
-let wind;
+let mover;
 
 function setup() {
   setCanvasContainer('canvas', 3, 2, true);
-  //   background('salmon');
-  ball = new Mover(width / 3, 0, 50);
-  ball2 = new Mover((2 * width) / 3, 0, 10);
-  gravity = createVector(0, 0.1);
-  wind = createVector(-1, -0);
+  background(255);
+  mover = new MoveNoMass(width / 2, height / 2, 50);
 }
+
 function draw() {
-  let g = p5.Vector.mult(gravity, ball.mass);
-  ball.applyForce(g);
-  let g2 = p5.Vector.mult(gravity, ball2.mass);
-  ball2.applyForce(g2);
-  //   ball.applyForce(gravity);
-  //   ball2.applyForce(gravity);
-  if (mouseIsPressed) {
-    ball.applyForce(wind);
-    ball2.applyForce(wind);
-  }
-  ball.update();
-  ball2.update();
-  ball.edgeBounce();
-  ball2.edgeBounce();
-  background('beige');
-  noStroke();
-  fill('yellowgreen');
-  ball.display();
-  ball2.display();
+  background(255);
+  mover.update();
+  mover.checkEdges();
+  mover.display();
+  mover.displayVectors();
 }
